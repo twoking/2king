d5c3361cf7eff77f4043a55aeba06ca2019ed58f
Prima Aulia Gusta <primaulia@gmail.com>
2019-06-18 16:56:52 +0800

    stable push for second and third degree friend list

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   has_many :friendships, class_name: "Friendship",
                         foreign_key: "follower_id",
                         dependent: :destroy
+  has_many :passive_friendships, class_name:  "Friendship",
+                                foreign_key: "followed_id",
+                                dependent:   :destroy
+
+  has_many :followings, through: :friendships, source: :followed
+  has_many :followers, through: :passive_friendships, source: :follower
 
   # validation
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -19,6 +25,38 @@ class User < ApplicationRecord
   # callbacks
   before_save { self.email = email.downcase }
 
+  # instance methods
+  def follow(another_user)
+    followings << another_user
+  end
+
+  def unfollow(following_user)
+    followings.delete(following_user)
+  end
+
+  def following?(another_user)
+    followings.include?(another_user)
+  end
+
+  def follower?(another_user)
+    followers.include?(another_user)
+  end
+
+  def second_degree_followings
+    combined_followings = []
+    combined_followings << followings
+    combined_followings << followings.map(&:followings)
+    combined_followings.flatten.uniq - [self]
+  end
+
+  def third_degree_followings
+    combined_followings = []
+    combined_followings << second_degree_followings
+    combined_followings << second_degree_followings.map(&:followings)
+    combined_followings.flatten.uniq - [self]
+  end
+
+  # class methods
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
