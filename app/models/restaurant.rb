ffc22fae7f69a4d937ee0dd420e3f119939e448f
guido caldara <guidocaldara@gmail.com>
2019-07-01 11:07:18 +0800

    showpage-frontend

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,11 +8,21 @@ class Restaurant < ApplicationRecord
     today_schedule = self.opening_hours.select {|d| d.include?(today_day)}.first.split(" ")
     morning_opening = today_schedule[1].to_i
     evening_close = today_schedule[-2].to_i + 12
-    (morning_opening..evening_close).include?(Time.now.hour)
+    if (morning_opening..evening_close).include?(Time.now.hour)
+      return "OPEN"
+    else
+      return "CLOSE"
+    end
   end
 
   #List ID based on current_user and restaurant
   def user_list(user)
     List.find_by(restaurant: self, user: user)
+  end
+
+  def display_price_level
+    string = ""
+    self.price_level.to_i.times { string.concat("$") }
+    string
   end
 end
