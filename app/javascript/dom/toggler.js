c3d2a4edf6ee01d1180e8a00fde21aa5d6f10c93
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-09-05 22:50:16 +0700

    navtabs color on active

--- app/javascript/dom/toggler.js
+++ app/javascript/dom/toggler.js
@@ -1,25 +1,37 @@
-const $panel = $('.panel')
-const $toggler = $('.toggler')
-const $filterNav = $('#bottom-nav-filter')
-const $homeNav = $('#bottom-nav-home')
+const $panel = $(".panel");
+const $toggler = $(".toggler");
+const $filterNav = $("#bottom-nav-filter");
+const $homeNav = $("#bottom-nav-home");
+const $listToggle = $("[data-target=degree-filter]");
+const $friendToggle = $("[data-target=friend-filter]");
 
 // maybe need refactoring
 
-$toggler.on('click', function(e) {
-  e.preventDefault();
-  const $target = $('#' + this.dataset.target).addClass('active');
-  $panel.not($target).removeClass('active');
+$toggler.on("click", function(e) {
+	e.preventDefault();
+	const $target = $("#" + this.dataset.target).addClass("active");
+	$panel.not($target).removeClass("active");
 
-  $filterNav.removeClass('active');
-  $homeNav.addClass('active');
+	if (e.currentTarget === $listToggle[0]) {
+		$friendToggle[0].classList.add("text-secondary");
+		$friendToggle[0].parentElement.classList.add("btn-home-gray");
+		$listToggle[0].classList.remove("text-secondary");
+		$listToggle[0].parentElement.classList.remove("btn-home-gray");
+	} else {
+		$listToggle[0].classList.add("text-secondary");
+		$listToggle[0].parentElement.classList.add("btn-home-gray");
+		$friendToggle[0].classList.remove("text-secondary");
+		$friendToggle[0].parentElement.classList.remove("btn-home-gray");
+	}
+
+	$filterNav.removeClass("active");
+	$homeNav.addClass("active");
 });
 
-$homeNav.on('click', function(e) {
-  $filterNav.addClass('active');
-  $homeNav.removeClass('active');
+$homeNav.on("click", function(e) {
+	$filterNav.addClass("active");
+	$homeNav.removeClass("active");
 
-  $panel.slice(1).removeClass('active');
-  $panel.first().addClass('active');
+	$panel.slice(1).removeClass("active");
+	$panel.first().addClass("active");
 });
-
-
