ddd7c4732d7226e8d0cdb01c7cbb49e647e4b26c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-09-05 12:30:01 +0700

    error handling for checkbox js bit

--- app/javascript/dom/checkbox.js
+++ app/javascript/dom/checkbox.js
@@ -1,10 +1,12 @@
 const degreeCheckboxes = document.querySelectorAll(".regular-checkbox");
-degreeCheckboxes.forEach(checkbox => {
-	if (checkbox.checked) {
-		checkbox.nextElementSibling.classList.add("font-weight-bold");
-	}
+if (degreeCheckboxes) {
+	degreeCheckboxes.forEach(checkbox => {
+		if (checkbox.checked) {
+			checkbox.nextElementSibling.classList.add("font-weight-bold");
+		}
 
-	checkbox.addEventListener("click", e => {
-		e.currentTarget.nextElementSibling.classList.toggle("font-weight-bold");
+		checkbox.addEventListener("click", e => {
+			e.currentTarget.nextElementSibling.classList.toggle("font-weight-bold");
+		});
 	});
-});
+}
