46759c49cc33b1dcdb29c1489c47c53accc009be
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-09-06 13:14:41 +0700

    will toggle on first visit and every 5th time they visit

--- app/javascript/dom/modal-toggle.js
+++ app/javascript/dom/modal-toggle.js
@@ -1,6 +1,20 @@
 const modalToggler = document.getElementById("modal-toggler");
-if (modalToggler && !navigator.userAgent.includes("Mobile")) {
+const toggleModal = toggler => {
 	document.addEventListener("DOMContentLoaded", () => {
-		modalToggler.click();
+		toggler.click();
 	});
+};
+
+if (!localStorage.visited) {
+	localStorage.visited = 0;
+	toggleModal(modalToggler);
+}
+
+if (modalToggler && !navigator.userAgent.includes("Mobile")) {
+	if (Number(localStorage.visited) === 4) {
+		toggleModal(modalToggler);
+		localStorage.visited = 0;
+	}
+
+	localStorage.visited = Number(localStorage.visited) + 1;
 }
