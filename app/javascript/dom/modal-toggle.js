4caeaa32b8cc236e46c3771b0b420552af7d8eab
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-09-09 19:50:09 +0700

    pwa checker

--- app/javascript/dom/modal-toggle.js
+++ app/javascript/dom/modal-toggle.js
@@ -5,16 +5,19 @@ const toggleModal = toggler => {
 	});
 };
 
-if (!localStorage.visited) {
-	localStorage.visited = 0;
-	toggleModal(modalToggler);
-}
-
-if (modalToggler && !navigator.userAgent.includes("Mobile")) {
-	if (Number(localStorage.visited) === 4) {
-		toggleModal(modalToggler);
+// If app is run on non-PWA, run everything
+if (!window.matchMedia("(display-mode: standalone)").matches) {
+	if (!localStorage.visited) {
 		localStorage.visited = 0;
+		toggleModal(modalToggler);
 	}
 
-	localStorage.visited = Number(localStorage.visited) + 1;
+	if (modalToggler && !navigator.userAgent.includes("Mobile")) {
+		if (Number(localStorage.visited) === 4) {
+			toggleModal(modalToggler);
+			localStorage.visited = 0;
+		}
+
+		localStorage.visited = Number(localStorage.visited) + 1;
+	}
 }
