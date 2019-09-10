8951899b3106d1fc5ecd24dd11d307016a115a03
guido caldara <guidocaldara@gmail.com>
2019-09-10 09:50:20 +0800

    nogo

--- app/javascript/components/append-restaurant.js
+++ app/javascript/components/append-restaurant.js
@@ -1,39 +1,31 @@
 import haversine from "haversine"
 
 const restaurantCard = (resto) => {
-  let isOpen = ""
-  const priceRange = resto.price_level ? "$".repeat(resto.price_level) : ""
-  if (resto.opening_hours){
-    isOpen = resto.opening_hours.open_now ? "OPEN" : "CLOSE"
+  const restaurant = resto.restaurant || resto
+  const openNow = ""
+  let distance = new Number;
+  const priceRange = restaurant.price_range ? "$".repeat(restaurant.price_range) : ""
+  if (restaurant.opening_hours){
+    const openNow = restaurant.opening_hours.open_now ? "OPEN" :  "CLOSED"
   }
   const mylat = localStorage.getItem('lat');
   const mylng = localStorage.getItem('lng');
   const start = { latitude: mylat, longitude: mylng };
-  const end = { latitude: resto.latitude, longitude: resto.longitude};
-  const picture = resto.cover_picture
-  if (resto.cover_picture == undefined){
-    const picture = resto.photos ? resto.photos[0].getUrl([200, 200]) : resto.icon
-  }
-  if (resto.latitude == undefined){
-    const end = { latitude: resto.geometry.location.lat(), longitude: resto.geometry.location.lng()}
-  }
-  return(
-    `<a href="/restaurants/${resto.place_id}">
-    <div class="card-product my-2">
-      <img src="${picture}">
-      <div class="card-product-infos">
-        <h2>${resto.name}</h2>
-        <p class="my-1">${haversine(start, end).toFixed(2)}Km away ${isOpen}</p>
-        <p class="my-0 mr-1">${isOpen} | ${priceRange}</p>
-      </div>
+  const end = { latitude: restaurant.latitude, longitude: restaurant.longitude};  
+  let dist = haversine(start, end).toFixed(2)
+  dist = dist.toString()[0] == "0" ? `${dist * 1000}m from you` : `${dist}Km from you`;
+  const picture = restaurant.thumb || ""
+  return (  `<a href="/restaurant?id=${restaurant.R.res_id}">
+  <div class="card-product my-2">
+    <img src="${picture}">
+    <div class="card-product-infos">
+      <h2>${restaurant.name}</h2>
+      <p class="my-1">${dist}</p>
+      <p class="my-0 mr-1">${priceRange}</p>
     </div>
-  </a>`
-
-  )
+  </div>
+</a>`)
 }
-
-
-
 
 const appendRestaurant = (result) =>{
   const searchBox = document.querySelector("#search-results")
@@ -41,3 +33,46 @@ const appendRestaurant = (result) =>{
 }
 
 export { appendRestaurant }
+// import haversine from "haversine"
+
+// const restaurantCard = (resto) => {
+//   let isOpen = ""
+//   const priceRange = resto.price_level ? "$".repeat(resto.price_level) : ""
+//   if (resto.opening_hours){
+//     isOpen = resto.opening_hours.open_now ? "OPEN" : "CLOSE"
+//   }
+//   const mylat = localStorage.getItem('lat');
+//   const mylng = localStorage.getItem('lng');
+//   const start = { latitude: mylat, longitude: mylng };
+//   const end = { latitude: resto.latitude, longitude: resto.longitude};
+//   const picture = resto.cover_picture
+//   if (resto.cover_picture == undefined){
+//     const picture = resto.photos ? resto.photos[0].getUrl([200, 200]) : resto.icon
+//   }
+//   if (resto.latitude == undefined){
+//     const end = { latitude: resto.geometry.location.lat(), longitude: resto.geometry.location.lng()}
+//   }
+//   return(
+//     `<a href="/restaurants/${resto.place_id}">
+//     <div class="card-product my-2">
+//       <img src="${picture}">
+//       <div class="card-product-infos">
+//         <h2>${resto.name}</h2>
+//         <p class="my-1">${haversine(start, end).toFixed(2)}Km away ${isOpen}</p>
+//         <p class="my-0 mr-1">${isOpen} | ${priceRange}</p>
+//       </div>
+//     </div>
+//   </a>`
+
+//   )
+// }
+
+
+
+
+// const appendRestaurant = (result) =>{
+//   const searchBox = document.querySelector("#search-results")
+//   searchBox.insertAdjacentHTML("beforeend", restaurantCard(result))
+// }
+
+// export { appendRestaurant }
--- app/javascript/components/restaurants-nearby.js
+++ app/javascript/components/restaurants-nearby.js
@@ -35,44 +35,68 @@ const getCurrentLocation = (callback) => {
 
 
 const getRestaurantsNearBy = (lat, lng, name = "") => {
-  document.querySelector("#search-results").innerHTML = ""
-  var request = {
-    location: new google.maps.LatLng(lat, lng),
-    radius: "1500",
-    type: ['restaurant'],
-    rankby: "distance"
-  };
-  var service = new google.maps.places.PlacesService(document.createElement('div'));
-  service.nearbySearch(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      const mylat = localStorage.getItem('lat');
-      const mylng = localStorage.getItem('lng');
-      const start = {
-        latitude: mylat,
-        longitude: mylng
-      };
-      results.forEach((resto) => {
-        resto.latitude =  resto.geometry.location.lat()
-        resto.longitude =  resto.geometry.location.lng()
-        const end = {
-          latitude: resto.latitude,
-          longitude: resto.longitude
-        }
+   document.querySelector("#search-results").innerHTML = ""
+   const mylat = localStorage.getItem('lat');
+   const mylng = localStorage.getItem('lng');
+   document.querySelector("#search-results").innerHTML = ""
+    const url = `https://developers.zomato.com/api/v2.1/search?count=25&lat=${lat}&lon=${lng}&radius=1000&establishment_type=31&sort=real_distance&order=asc`
+   fetch(url, {headers: { "user-key": "55b999fc85f2826feadca8e1d2a52a83" }})
+   .then(response => response.json())
+   .then((data) => {
+     data.restaurants.forEach((result) => {
+        let resto = result.restaurant;
+        const start = { latitude: mylat, longitude: mylng };
+        resto.latitude =  parseFloat(resto.location.latitude)
+        resto.longitude =  parseFloat(resto.location.longitude)
+        const end = { latitude: resto.latitude, longitude: resto.longitude }
         resto.distance_from_me = parseFloat(haversine(start, end).toFixed(2))
-        resto.cover_picture = (resto.photos !== undefined) ? resto.photos[0].getUrl([200, 200]) : resto.icon
-      })
-    };
-    results.sort((a, b) => {
-      return a.distance_from_me - b.distance_from_me
-    });
-    localStorage.setItem('restaurants', JSON.stringify(results))
-    results.forEach((result) => {
-      appendRestaurant(result)
-    })
-  })
+        resto.cover_picture = resto.featured_image
+     });
+     data.restaurants.sort((a, b) => { return a.restaurant.distance_from_me - b.restaurant.distance_from_me });
+     localStorage.setItem('restaurants', JSON.stringify(data.restaurants))
+     data.restaurants.forEach((result) => {
+       appendRestaurant(result.restaurant)
+     })
+ 
+   });
+  // var request = {
+  //   location: new google.maps.LatLng(lat, lng),
+  //   radius: "1500",
+  //   type: ['restaurant'],
+  //   rankby: "distance"
+  // };
+  // var service = new google.maps.places.PlacesService(document.createElement('div'));
+  // service.nearbySearch(request, function(results, status) {
+  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //     const mylat = localStorage.getItem('lat');
+  //     const mylng = localStorage.getItem('lng');
+  //     const start = {
+  //       latitude: mylat,
+  //       longitude: mylng
+  //     };
+  //     results.forEach((resto) => {
+  //       resto.latitude =  resto.geometry.location.lat()
+  //       resto.longitude =  resto.geometry.location.lng()
+  //       const end = {
+  //         latitude: resto.latitude,
+  //         longitude: resto.longitude
+  //       }
+  //       resto.distance_from_me = parseFloat(haversine(start, end).toFixed(2))
+  //       resto.cover_picture = (resto.photos !== undefined) ? resto.photos[0].getUrl([200, 200]) : resto.icon
+  //     })
+  //   };
+  //   results.sort((a, b) => {
+  //     return a.distance_from_me - b.distance_from_me
+  //   });
+  //   localStorage.setItem('restaurants', JSON.stringify(results))
+  //   results.forEach((result) => {
+  //     appendRestaurant(result)
+  //   })
+  // })
 
 
 }
+
 
 const listRestaurants = () => {
   getCurrentLocation((newPosition) => {
