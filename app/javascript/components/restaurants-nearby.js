d858d30a60c9e34daeb529f8b05b86e43e30eaa4
guido caldara <guidocaldara@gmail.com>
2019-10-10 12:23:49 +0800

    ready with zoomato

--- app/javascript/components/restaurants-nearby.js
+++ app/javascript/components/restaurants-nearby.js
@@ -39,7 +39,7 @@ const getRestaurantsNearBy = (lat, lng, name = "") => {
    const mylat = localStorage.getItem('lat');
    const mylng = localStorage.getItem('lng');
    document.querySelector("#search-results").innerHTML = ""
-    const url = `https://developers.zomato.com/api/v2.1/search?count=25&lat=${lat}&lon=${lng}&radius=1000&establishment_type=31&sort=real_distance&order=asc`
+    const url = `https://developers.zomato.com/api/v2.1/search?count=25&lat=${lat}&lon=${lng}&radius=200&sort=real_distance&order=asc`
    fetch(url, {headers: { "user-key": "55b999fc85f2826feadca8e1d2a52a83" }})
    .then(response => response.json())
    .then((data) => {
@@ -54,47 +54,11 @@ const getRestaurantsNearBy = (lat, lng, name = "") => {
      });
      data.restaurants.sort((a, b) => { return a.restaurant.distance_from_me - b.restaurant.distance_from_me });
      localStorage.setItem('restaurants', JSON.stringify(data.restaurants))
+     console.log(data.restaurants)
      data.restaurants.forEach((result) => {
        appendRestaurant(result.restaurant)
      })
- 
    });
-  // var request = {
-  //   location: new google.maps.LatLng(lat, lng),
-  //   radius: "1500",
-  //   type: ['restaurant'],
-  //   rankby: "distance"
-  // };
-  // var service = new google.maps.places.PlacesService(document.createElement('div'));
-  // service.nearbySearch(request, function(results, status) {
-  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //     const mylat = localStorage.getItem('lat');
-  //     const mylng = localStorage.getItem('lng');
-  //     const start = {
-  //       latitude: mylat,
-  //       longitude: mylng
-  //     };
-  //     results.forEach((resto) => {
-  //       resto.latitude =  resto.geometry.location.lat()
-  //       resto.longitude =  resto.geometry.location.lng()
-  //       const end = {
-  //         latitude: resto.latitude,
-  //         longitude: resto.longitude
-  //       }
-  //       resto.distance_from_me = parseFloat(haversine(start, end).toFixed(2))
-  //       resto.cover_picture = (resto.photos !== undefined) ? resto.photos[0].getUrl([200, 200]) : resto.icon
-  //     })
-  //   };
-  //   results.sort((a, b) => {
-  //     return a.distance_from_me - b.distance_from_me
-  //   });
-  //   localStorage.setItem('restaurants', JSON.stringify(results))
-  //   results.forEach((result) => {
-  //     appendRestaurant(result)
-  //   })
-  // })
-
-
 }
 
 
